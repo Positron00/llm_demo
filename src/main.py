73dcd2a53beb83e4d0cf8950e5229190fe67329c
Chuntao Dan <danc@janelia.hhmi.org>
2024-09-25 20:51:07 -0400

    setting up main.py

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import argparse
+
+# parse the input argument
+parser = argparse.ArgumentParser()
+parser.add_argument('--model')
+parser.add_argument('--dataset')
+args = parser.parse_args()
+
+# download models and datasets
+def cache():
+    models = []
+    datasets = []
+
+# run a task
+def run():
+    pass
+
+if __name__ == '__main__':
+    run()
