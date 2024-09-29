5fc162034f540c59c2a921ea40dd1aaf5d927d26
Positron00 <161525700+Positron00@users.noreply.github.com>
2024-09-29 19:27:31 -0400

    set up tests.py; test number of parameters to be finetuned

---
+++ src/tests.py
@@ -0,0 +1,26 @@
+import os
+import torch
+import transformers
+
+import utils
+import ft
+
+os.environ["DEVICE"] = "cpu"
+DEVICE = torch.device("cpu")
+
+
+class Test_ft_params():
+    def setUp(self):
+        # Cache the datasets and models needed
+        utils.get_model_and_tokenizer('bert-tiny', transformers.AutoModelForCausalLM)
+        utils.get_model_and_tokenizer('bert-tiny', transformers.AutoModelForSequenceClassification, num_labels=5)
+        utils.get_dataset(dataset='amazon', n_train=1, n_val=125)
+
+    def test(self):
+        """Basic test case for testing the number of parameters to fine tune in mode 'all'."""
+        
+        model, _ = utils.get_model_and_tokenizer('bert-tiny', transformers.AutoModelForCausalLM)
+        parameters = [parameter for parameter in ft.parameters_to_fine_tune(model, "all")]
+
+        # Check that the number of parameters to be optimized match
+        self.assertTrue(len(parameters) == 42, "Incorrect number of parameters to be optimized returned by parameters_to_fine_tune!")
