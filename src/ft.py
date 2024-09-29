011817e6e983d60bb59103e32971bdad23169657
Positron00 <161525700+Positron00@users.noreply.github.com>
2024-09-28 21:32:13 -0400

    finetune different models separately; update get_loss(), get_acc(); add eval()

--- src/ft.py
+++ src/ft.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 import torch
 import torch.nn as nn
@@ -15,7 +15,10 @@ from collections import defaultdict
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 import itertools
+from icl import get_icl_prompts, do_sample, get_performance_metric
 import tqdm
+import random
+
 
 # determine device to train on
 DEVICE = os.environ["DEVICE"] if "DEVICE" in os.environ else "cpu"
@@ -31,11 +34,12 @@ print("Fine-tuning using device: ", DEVICE)
 
 
 class LoRAConv1DWrapper(nn.Module):
+    # set up LoRA-augmented layer
     def __init__(self, conv1dmodule: nn.Module, lora_rank: int):
         super().__init__()
 
         self.base_module = conv1dmodule
-             
+        
         for param in self.base_module.parameters():
             param.requires_grad = False
 
@@ -56,7 +60,7 @@ def parameters_to_fine_tune(model: nn.Module, mode: str) -> List:
     Args:
       model: the model we're fine-tuning
       mode: the fine-tuning mode we're using; may be 'all', 'last', 'first',
-        'middle'
+        'middle', or 'loraN' (where N is an integer)
     
     Returns:
       A list of nn.Parameters of `model` that should be fine-tuned in the given
@@ -65,6 +69,7 @@ def parameters_to_fine_tune(model: nn.Module, mode: str) -> List:
 
     if mode == 'all':
         return model.parameters()
+    
     elif mode == 'last':
         for param in model.parameters():
             param.requires_grad = False
@@ -82,16 +87,22 @@ def parameters_to_fine_tune(model: nn.Module, mode: str) -> List:
             param.requires_grad = True
 
         return filter(lambda p: p.requires_grad, model.parameters())
+    
     elif mode == 'middle':
         for param in model.parameters():
             param.requires_grad = False
 
         transformer_blocks = model.transformer.h
         mid_index = len(transformer_blocks) // 2
+
+        #for param in model.transformer.h[(mid_index-1):(mid_index+1)].parameters():
+        #    param.requires_grad = True
+        #parameters = filter(lambda p: p.requires_grad, model.parameters())
+        
         parameters = list(transformer_blocks[mid_index - 1].parameters()) + list(transformer_blocks[mid_index].parameters())
 
         return parameters
-    
+
     elif mode.startswith('lora'):
         for param in model.parameters():
             param.requires_grad = False
@@ -103,51 +114,110 @@ def parameters_to_fine_tune(model: nn.Module, mode: str) -> List:
                         param.requires_grad = True
 
         return filter(lambda p: p.requires_grad, model.parameters())
-    
+
     else:
         raise NotImplementedError()
 
 
 def get_loss(logits: torch.tensor, targets: torch.tensor) -> torch.tensor:
     """
-    Computes the cross-entropy loss.
+    Computes the cross-entropy loss for either sequence classification or generation.
+
+    For generation, note different sequences within the batch have different lengths, 
+      and the targets tensor includes some mask values (-100). The average loss is the 
+      *average loss over all non-masked timesteps*. The prediction for what token t will 
+      be is made after seeing only t - 1 tokens; that is, there is an off-by-one shift 
+      needed between the logits and targets.
 
     Args:
-      logits: a 2D [batch_size, n_classes] (for classification) 
-      targets: a 1D [batch_size] (for classification) tensor of target indices. 
-
+      logits: a 2D [batch_size, n_classes] (for classification) or 3D
+        [batch_size, sequence_length, vocab_size] (for generation) tensor
+        of *UNNORMALIZED* logits
+      targets: a 1D [batch_size] (for classification) or 2D [batch_size, sequence_length]
+        (for generation) tensor of target indices. For the generation case, may contain
+        -100 in some positions, meaning that the loss for this timestep should be ignored.
+    
     Returns:
       A zero-dim tensor representing the average cross-entropy loss over all batch 
         elements (and sequence timesteps, if applicable)
     """
 
-    return F.cross_entropy(logits, targets).mean()
+    if logits.dim() == 2:
+        loss = F.cross_entropy(logits, targets).mean()
+
+    elif logits.dim() == 3:
+        logits = logits[:, :-1, :].contiguous()
+        targets = targets[:, 1:].contiguous()
+
+        logits = logits.view(-1, logits.size(-1))
+        targets = targets.view(-1)
+
+        loss = F.cross_entropy(logits, targets, ignore_index=-100)
+
+    else:
+        raise ValueError(f'Logits should either be 2-dim (for classification) or 3-dim (for generation); got {logits.dim()}')
+
+    return loss
+
 
 def get_acc(logits, targets):
     """
-    Computes the exact match accuracy 
+    Computes the exact match accuracy for either sequence classification or generation. i.e.,
+      the fraction of predictions for which the most likely class/token equals the target.
+
+    For generation, need to deal with the fact that different sequences within
+      the batch have different lengths, and the targets tensor includes some mask
+      values (-100). The average accuracy is the *average accuracy over all non-masked timesteps*.
+      Also need to handle the fact that the prediction for what token t will be is
+      made after seeing only t - 1 tokens; that is, there is an off-by-one shift needed
+      between the logits and targets.
 
     Args:
-      logits: a 2D [batch_size, n_classes] (for classification) tensor of logits
-      targets: a 1D [batch_size] (for classification) tensor of target indices. 
+      logits: a 2D [batch_size, n_classes] (for classification) or 3D
+        [batch_size, sequence_length, vocab_size] (for generation) tensor of logits
+      targets: a 1D [batch_size] (for classification) or 2D [batch_size, sequence_length]
+        (for generation) tensor of target indices. For the generation case, may contain
+        -100 in some positions, meaning that the loss for this timestep should be ignored.
     
     Returns:
       A *scalar* representing the average exact-match accuracy over all non-masked batch 
-        elements
+        elements (and sequence timesteps, if applicable)
     """
 
-    y = torch.argmax(logits, dim=-1) == targets
-    y = y.type(torch.float)
-    return torch.mean(y).item()
+    if logits.dim() == 2:
+        y = torch.argmax(logits, dim=-1) == targets
+        y = y.type(torch.float)
+        return torch.mean(y).item()
     
+    elif logits.dim() == 3:
+        logits = logits[:, :-1, :].contiguous()
+        targets = targets[:, 1:].contiguous()
 
-def ft_llm(model, tokenizer, x, y, mode, debug, batch_size=8):
+        y = torch.argmax(logits, dim=-1)
+        mask = (targets != -100).float()
+        correct = (y == targets).float() * mask
+
+        return (correct.sum() / mask.sum()).item()
+    
+    else:
+        raise ValueError(f'Logits should either be 2-dim (for classification) or 3-dim (for generation); got {logits.dim()}')
+
+
+def ft_bert(model, tokenizer, x, y, mode, debug, batch_size=8):
     model = copy.deepcopy(model)
+
+    if mode.startswith('lora'):
+        for m in model.transformer.h:
+            m.mlp.c_fc = LoRAConv1DWrapper(m.mlp.c_fc, int(mode[4:]))
+            m.mlp.c_proj = LoRAConv1DWrapper(m.mlp.c_proj, int(mode[4:]))
+
+    model.to(DEVICE)
 
     optimizer = torch.optim.Adam(parameters_to_fine_tune(model, mode), lr=1e-4)
     all_x = tokenizer(x, return_tensors='pt', padding=True, truncation=True, max_length=100).to(DEVICE)
     all_y = torch.tensor(y, device=DEVICE)
     pbar = tqdm.tqdm(range(1000))
+
     for step in pbar:
         batch = np.random.randint(0, len(x), batch_size)
         x_ = tokenizer([x[i] for i in batch], return_tensors='pt', padding=True, truncation=True, max_length=100).to(DEVICE)
@@ -157,6 +227,7 @@ def ft_llm(model, tokenizer, x, y, mode, debug, batch_size=8):
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
+
         if debug:
             break
 
@@ -166,7 +237,138 @@ def ft_llm(model, tokenizer, x, y, mode, debug, batch_size=8):
             pbar.set_description(f'Fine-tuning acc: {total_acc:.04f}')
             if total_acc > 0.75:
                 break
+
     return model
+
+
+def tokenize_gpt2_batch(tokenizer, x, y):
+    """
+    Implement the tokenization step for a batch of examples for GPT-2.
+
+    Args:
+        tokenizer: a GPT2Tokenizer that one can call and receive a dictionary of:
+          - input_ids: a list (or tensor) of token ids
+          - attention_mask: a list (or tensor) of 1s and 0s indicating which tokens
+              are padding (if you requested padding and tensors from the tokenizer)
+        x: a list of strings, each of which is the input for a single example
+        y: a list of strings, each of which is a *target* for a single example
+    
+    Returns:
+        A dictionary with the following keys:
+            - input_ids: a tensor of shape [batch_size, sequence_length] 
+                containing the token ids
+            - attention_mask: a tensor of shape [batch_size, sequence_length] 
+                containing 1s and 0s indicating which tokens are padding
+            - labels: a tensor of shape [batch_size, sequence_length] containing
+                the target token ids, with -100 for non-target tokens (i.e., the
+                tokens in the input part of each example or padding tokens)
+        where sequence_length is determined by the (x, y) pair whose tokenized
+        length is the longest in the batch. The other sequences should be padded to
+        this length (the tokenizer can handle this padding).
+    """
+
+    combined_sequences = [x_ + y_ for x_, y_ in zip(x, y)]
+    tokenized_sequences = tokenizer(combined_sequences,return_tensors="pt",padding=True,truncation=True)
+
+    labels = torch.full_like(tokenized_sequences['input_ids'], -100)
+    tokenized_targets = tokenizer(y,return_tensors="pt",padding=True,truncation=True)
+    tokenized_inputs = tokenizer(x,return_tensors="pt",padding=True,truncation=True)
+
+    for i in range(len(labels)):
+        inputTokens = tokenized_inputs['input_ids'][i]
+        targetTokens = tokenized_targets['input_ids'][i]
+        inputMask = tokenized_inputs['attention_mask'][i]
+        targetMask = tokenized_targets['attention_mask'][i]
+
+        lenInput = sum(inputMask)
+        lenTarget = sum(targetMask)
+
+        for j in range(lenInput,(lenInput+lenTarget)):
+            if targetMask[j-lenInput] == 1:
+                labels[i,j] = targetTokens[j-lenInput]
+
+    tokenized_sequences['labels'] = labels
+    
+    return tokenized_sequences.to(DEVICE)
+
+
+def add_prefixes(x: List[str], y: List[str], dataset: str) -> Tuple[List[str], List[str]]:
+    input_prefix = '' if utils.is_qa_dataset(dataset) else ''
+    label_prefix = ' In the' if utils.is_qa_dataset(dataset) else ' TL;DR:'
+    label_suffix = '.' if utils.is_qa_dataset(dataset) else ''
+
+    x = [input_prefix + x_.replace('\n', ' ') + label_prefix for x_ in x]
+    y = [' ' + y_.replace('\n', ' ') + label_suffix for y_ in y]
+
+    return x, y
+
+
+def ft_gpt2(model, tokenizer, x, y, mode, dataset, batch_size=8, grad_accum=8):
+    x, y = add_prefixes(x, y, dataset)
+    model = copy.deepcopy(model)
+
+    if mode.startswith('lora'):
+        for m in model.transformer.h:
+            m.mlp.c_fc = LoRAConv1DWrapper(m.mlp.c_fc, int(mode[4:]))
+            m.mlp.c_proj = LoRAConv1DWrapper(m.mlp.c_proj, int(mode[4:]))
+            m.attn.c_attn = LoRAConv1DWrapper(m.attn.c_attn, int(mode[4:]))
+
+    model.to(DEVICE)
+    optimizer = torch.optim.Adam(parameters_to_fine_tune(model, mode), lr=2e-5)
+
+    all_both = tokenize_gpt2_batch(tokenizer, x, y)
+    max_n = len(x) * 10
+    pbar = tqdm.tqdm(range(max_n))
+    idxs = []
+    for step in pbar:
+        model.train()
+
+        if len(idxs) < batch_size // grad_accum:
+            idxs = list(range(len(x)))
+            random.shuffle(idxs)
+        batch_idxs = idxs[:batch_size // grad_accum]
+        idxs = idxs[batch_size // grad_accum:]
+
+        x_batch = [x[i] for i in batch_idxs]
+        y_batch = [y[i] for i in batch_idxs]
+
+        batch = tokenize_gpt2_batch(tokenizer, x_batch, y_batch)
+        model_output = model(**batch, use_cache=False)
+        loss = get_loss(model_output.logits, batch['labels'])
+        loss = loss / grad_accum
+        loss.requires_grad = True
+        loss.backward()
+
+        if (step + 1) % grad_accum == 0:
+            optimizer.step()
+            optimizer.zero_grad()
+
+        pbar.set_postfix(loss=loss.item())
+
+        if step % (grad_accum * 5) == 0:
+            with torch.inference_mode():
+                model.eval()
+                accs = []
+                for idx in range(len(list(all_both.values())[0])):
+                    d = {k: v[idx:idx+1] for k, v in all_both.items()}
+                    acc = get_acc(model(**d).logits, d['labels'])
+                    accs.append(acc)
+                total_acc = sum(accs) / len(accs)
+                pbar.set_description(f'Fine-tuning acc: {total_acc:.04f}')
+
+            if total_acc >= utils.early_stop_thresold(dataset):
+                print('Early stopping!')
+                break
+
+    return model
+
+
+def eval(model, tok, val_data):
+    x = tok(val_data['x'], return_tensors='pt', padding=True, truncation=True, max_length=100).to(DEVICE)
+    y = torch.tensor(val_data['y'], device=DEVICE)
+    with torch.inference_mode():
+        logits = model(**x).logits
+    return get_acc(logits, y)
 
 
 def run_ft(models: List[str], datasets: List[str], ks: List[int], modes: List[str], debug: bool, repeats: int, n_val: int = 125):
@@ -199,15 +401,17 @@ def run_ft(models: List[str], datasets: List[str], ks: List[int], modes: List[st
 
                     if repeat > 0:
                         print(f'Beginning repeat #{repeat}')
+
                     if dataset == 'amazon':
-                        fine_tuned = ft_llm(model, tokenizer, train['x'][:k*5], train['y'][:k*5], mode, debug)
+                        fine_tuned = ft_bert(model, tokenizer, train['x'][:k*5], train['y'][:k*5], mode, debug)
                         val_acc = eval(fine_tuned, tokenizer, val)
 
                         if val_acc >= results.get(result_key, 0):
                             results[result_key] = val_acc
+
                     else:
                         if k > 0:
-                            fine_tuned = ft_llm(model, tokenizer, train['x'][:k], train['simple_y'][:k], mode, dataset)
+                            fine_tuned = ft_gpt2(model, tokenizer, train['x'][:k], train['simple_y'][:k], mode, dataset)
                         else:
                             fine_tuned = copy.deepcopy(model)
                             fine_tuned.to(DEVICE)
@@ -222,7 +426,7 @@ def run_ft(models: List[str], datasets: List[str], ks: List[int], modes: List[st
                             targets.append(val['y'][row])
                             max_tokens = utils.max_sampled_tokens_for_dataset(dataset)
                             prompt_mode = 'qa' if utils.is_qa_dataset(dataset) else 'tldr'
-                            prompt = get_prompts([], [], test_input, prompt_mode=prompt_mode)
+                            prompt = get_icl_prompts([], [], test_input, prompt_mode=prompt_mode)
                             input_ids = tokenizer(prompt, return_tensors='pt').input_ids.to(DEVICE)
                             sampled_tokens = do_sample(fine_tuned, input_ids, stop_tokens, max_tokens)
                             decoded = tokenizer.decode(sampled_tokens).strip()
@@ -266,6 +470,7 @@ def plot_ft(models, datasets, ks, modes, output):
 
     if max(x_vals) > 4:
         plt.xscale('symlog')
+        
     ax = plt.gca()
     ax.xaxis.set_major_formatter(mticker.ScalarFormatter())
     ax.xaxis.set_ticks(sorted(x_vals))
