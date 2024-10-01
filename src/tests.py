61adc17e7f7059ba30623dce0d93f77788c07fe2
Positron00 <161525700+Positron00@users.noreply.github.com>
2024-09-30 20:02:16 -0400

    add test for Test_ft_params; add *.pyc to .gitignore

--- src/tests.py
+++ src/tests.py
@@ -1,4 +1,4 @@
-import os
+import os, unittest
 import torch
 import transformers
 
@@ -9,7 +9,7 @@ os.environ["DEVICE"] = "cpu"
 DEVICE = torch.device("cpu")
 
 
-class Test_ft_params():
+class Test_ft_params(unittest.TestCase):
     def setUp(self):
         # Cache the datasets and models needed
         utils.get_model_and_tokenizer('bert-tiny', transformers.AutoModelForCausalLM)
@@ -21,6 +21,7 @@ class Test_ft_params():
         
         model, _ = utils.get_model_and_tokenizer('bert-tiny', transformers.AutoModelForCausalLM)
         parameters = [parameter for parameter in ft.parameters_to_fine_tune(model, "all")]
+        print(len(parameters))
 
         # Check that the number of parameters to be optimized match
         self.assertTrue(len(parameters) == 42, "Incorrect number of parameters to be optimized returned by parameters_to_fine_tune!")
