e73f9b0bfc6659724828ed99d916a84c287a0c05
Positron00 <161525700+Positron00@users.noreply.github.com>
2024-10-08 20:15:19 -0400

    add declaring loss.requires_grad=True and print it

--- src/ft.py
+++ src/ft.py
@@ -166,6 +166,8 @@ def get_loss(logits: torch.tensor, targets: torch.tensor) -> torch.tensor:
     else:
         raise ValueError(f'Logits should either be 2-dim (for classification) or 3-dim (for generation); got {logits.dim()}')
 
+    loss.requires_grad = True
+    print(loss)
     return loss
 
 
