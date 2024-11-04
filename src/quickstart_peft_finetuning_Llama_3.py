b76c412785b05640c53b0c625dcc58ed67b74a51
Positron00 <161525700+Positron00@users.noreply.github.com>
2024-11-04 17:25:20 -0500

    convert peft ipynb to .py

---
+++ src/quickstart_peft_finetuning_Llama_3.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+# 
+# <a href="https://colab.research.google.com/github/meta-llama/llama-recipes/blob/main/recipes/quickstart/finetuning/quickstart_peft_finetuning.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
+
+# ## PEFT Finetuning Quick Start Notebook
+# 
+# This notebook shows how to train a Meta Llama 3 model on a single GPU (e.g. A10 with 24GB) using int8 quantization and LoRA finetuning.
+# 
+# **_Note:_** To run this notebook on a machine with less than 24GB VRAM (e.g. T4 with 16GB) the context length of the training dataset needs to be adapted.
+# We do this based on the available VRAM during execution.
+# If you run into OOM issues try to further lower the value of train_config.context_length.
+
+# ### Step 0: Install pre-requirements and convert checkpoint
+# 
+# We need to have llama-recipes and its dependencies installed for this notebook. Additionally, we need to log in with the huggingface_cli and make sure that the account is able to to access the Meta Llama weights.
+
+# In[1]:
+
+
+# uncomment if running from Colab T4
+# ! pip install llama-recipes ipywidgets
+
+# import huggingface_hub
+# huggingface_hub.login()
+
+
+# ### Step 1: Load the model
+# 
+# Setup training configuration and load the model and tokenizer.
+
+# In[2]:
+
+
+import torch
+from transformers import LlamaForCausalLM, AutoTokenizer
+from llama_recipes.configs import train_config as TRAIN_CONFIG
+
+train_config = TRAIN_CONFIG()
+train_config.model_name = "meta-llama/Meta-Llama-3.1-8B"
+train_config.num_epochs = 1
+train_config.run_validation = False
+train_config.gradient_accumulation_steps = 4
+train_config.batch_size_training = 1
+train_config.lr = 3e-4
+train_config.use_fast_kernels = True
+train_config.use_fp16 = True
+train_config.context_length = 1024 if torch.cuda.get_device_properties(0).total_memory < 16e9 else 2048 # T4 16GB or A10 24GB
+train_config.batching_strategy = "packing"
+train_config.output_dir = "meta-llama-samsum"
+train_config.use_peft = True
+
+from transformers import BitsAndBytesConfig
+config = BitsAndBytesConfig(
+    load_in_8bit=True,
+)
+
+model = LlamaForCausalLM.from_pretrained(
+            train_config.model_name,
+            device_map="auto",
+            quantization_config=config,
+            use_cache=False,
+            attn_implementation="sdpa" if train_config.use_fast_kernels else None,
+            torch_dtype=torch.float16,
+        )
+
+tokenizer = AutoTokenizer.from_pretrained(train_config.model_name)
+tokenizer.pad_token = tokenizer.eos_token
+
+
+# ### Step 2: Check base model
+# 
+# Run the base model on an example input:
+
+# In[3]:
+
+
+eval_prompt = """
+Summarize this dialog:
+A: Hi Tom, are you busy tomorrow’s afternoon?
+B: I’m pretty sure I am. What’s up?
+A: Can you go with me to the animal shelter?.
+B: What do you want to do?
+A: I want to get a puppy for my son.
+B: That will make him so happy.
+A: Yeah, we’ve discussed it many times. I think he’s ready now.
+B: That’s good. Raising a dog is a tough issue. Like having a baby ;-) 
+A: I'll get him one of those little dogs.
+B: One that won't grow up too big;-)
+A: And eat too much;-))
+B: Do you know which one he would like?
+A: Oh, yes, I took him there last Monday. He showed me one that he really liked.
+B: I bet you had to drag him away.
+A: He wanted to take it home right away ;-).
+B: I wonder what he'll name it.
+A: He said he’d name it after his dead hamster – Lemmy  - he's  a great Motorhead fan :-)))
+---
+Summary:
+"""
+
+model_input = tokenizer(eval_prompt, return_tensors="pt").to("cuda")
+
+model.eval()
+with torch.inference_mode():
+    print(tokenizer.decode(model.generate(**model_input, max_new_tokens=100)[0], skip_special_tokens=True))
+
+
+# We can see that the base model only repeats the conversation.
+# 
+# ### Step 3: Load the preprocessed dataset
+# 
+# We load and preprocess the samsum dataset which consists of curated pairs of dialogs and their summarization:
+
+# In[4]:
+
+
+from llama_recipes.configs.datasets import samsum_dataset
+from llama_recipes.utils.dataset_utils import get_dataloader
+
+samsum_dataset.trust_remote_code = True
+
+train_dataloader = get_dataloader(tokenizer, samsum_dataset, train_config)
+eval_dataloader = get_dataloader(tokenizer, samsum_dataset, train_config, "val")
+
+
+# ### Step 4: Prepare model for PEFT
+# 
+# Let's prepare the model for Parameter Efficient Fine Tuning (PEFT):
+
+# In[5]:
+
+
+from peft import get_peft_model, prepare_model_for_kbit_training, LoraConfig
+from dataclasses import asdict
+from llama_recipes.configs import lora_config as LORA_CONFIG
+
+lora_config = LORA_CONFIG()
+lora_config.r = 8
+lora_config.lora_alpha = 32
+lora_dropout: float=0.01
+
+peft_config = LoraConfig(**asdict(lora_config))
+
+model = prepare_model_for_kbit_training(model)
+model = get_peft_model(model, peft_config)
+
+
+# ### Step 5: Fine tune the model
+# 
+# Here, we fine tune the model for a single epoch.
+
+# In[6]:
+
+
+import torch.optim as optim
+from llama_recipes.utils.train_utils import train
+from torch.optim.lr_scheduler import StepLR
+
+model.train()
+
+optimizer = optim.AdamW(
+            model.parameters(),
+            lr=train_config.lr,
+            weight_decay=train_config.weight_decay,
+        )
+scheduler = StepLR(optimizer, step_size=1, gamma=train_config.gamma)
+
+# Start the training process
+results = train(
+    model,
+    train_dataloader,
+    eval_dataloader,
+    tokenizer,
+    optimizer,
+    scheduler,
+    train_config.gradient_accumulation_steps,
+    train_config,
+    None,
+    None,
+    None,
+    wandb_run=None,
+)
+
+
+# ### Step 6:
+# Save model checkpoint
+
+# In[7]:
+
+
+model.save_pretrained(train_config.output_dir)
+
+
+# ### Step 7:
+# Try the fine tuned model on the same example again to see the learning progress:
+
+# In[8]:
+
+
+model.eval()
+with torch.inference_mode():
+    print(tokenizer.decode(model.generate(**model_input, max_new_tokens=100)[0], skip_special_tokens=True))
+
